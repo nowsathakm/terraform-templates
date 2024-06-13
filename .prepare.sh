f8ebc2cc36a14cffdacb24261831bc5ba3d54d8b
Nowsath <117368455+nowsathakm@users.noreply.github.com>
2024-06-13 11:14:18 +0530

    Update .prepare.sh

--- .prepare.sh
+++ .prepare.sh
@@ -4,8 +4,17 @@ set -e
 # remove scritp artifacts
 #rm -fr build.env backend.tf terraform.tfvars
 
-export TF_VAR_environment=${{ github.ref_name }}  # Remove leading 'refs/heads/'
-TF_VAR_environment="${TF_VAR_environment:-"develop"}"  # Set default to 'develop' if empty
+# ./.prepare.sh (modified line 7)
+if [[ $github.ref == 'refs/heads/main' ]]; then
+  export TF_VAR_environment="production"
+elif [[ $github.ref == 'refs/heads/staging' ]]; then
+  export TF_VAR_environment="staging"
+else
+  export TF_VAR_environment="develop"
+fi
+
+#export TF_VAR_environment=${{ github.ref_name }}  # Remove leading 'refs/heads/'
+#TF_VAR_environment="${TF_VAR_environment:-"develop"}"  # Set default to 'develop' if empty
 
 #TF_VAR_environment="${TF_VAR_environment:-"develop"}"
 echo "TF_VAR_environment=$TF_VAR_environment" >> build.env
