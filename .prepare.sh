0c8a0b59e2f7e21ef838401fc9b2703700a7acd3
Nowsath <117368455+nowsathakm@users.noreply.github.com>
2024-06-13 10:14:03 +0530

    Update .prepare.sh

--- .prepare.sh
+++ .prepare.sh
@@ -4,7 +4,7 @@ set -e
 # remove scritp artifacts
 #rm -fr build.env backend.tf terraform.tfvars
 
-export TF_VAR_environment=${{ github.ref == 'refs/heads/main' ? 'production' : github.ref == 'refs/heads/develop' ? 'develop' : 'staging' }}
+export TF_VAR_environment=${{ github.ref == 'refs/heads/main' ? 'production' : github.ref == 'refs/heads/staging' ? 'staging' : 'develop' }}
 
 TF_VAR_environment="${TF_VAR_environment:-"develop"}"
 echo "TF_VAR_environment=$TF_VAR_environment" >> build.env
